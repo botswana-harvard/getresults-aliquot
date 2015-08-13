3735cc1f9a332fbc43dcaed86773b05591ea6948
ckgathi <ckgathi@bhp.org.bw>
2015-08-13 16:29:36 +0200

    aliquot patient filter

--- getresults_aliquot/tests/test_aliquot_patient_filter.py
+++ getresults_aliquot/tests/test_aliquot_patient_filter.py
@@ -4,9 +4,8 @@ from django.utils import timezone
 
 from getresults_aliquot.models import Aliquot, AliquotType
 from getresults_receive.models import Receive, Patient
-from getresults_aliquot.admin import AliquotAdmin
-from getresults_aliquot.filters import AliquotPatientFilter
-
+from ..admin import AliquotAdmin
+from ..filters import AliquotPatientFilter
 
 class TestAliquotPaitentFilter(TestCase):
 
@@ -29,14 +28,16 @@ class TestAliquotPaitentFilter(TestCase):
         return AliquotType.objects.create(
             name='whole blood', alpha_code='WB', numeric_code='02')
 
-    @property
-    def aliquot(self):
+    def test_aliquot_patient_filter(self):
         """Test if the quesry set will filter by patient protocol."""
         Aliquot.objects.create(
             receive=self.receive,
             aliquot_identifier='AA3456700000201',
             aliquot_type=self.aliquot_type,
         )
-        filter = admin.AliquotPatientFilter(None, {'receive__patient__protocol': self.patient.protocol}, Aliquot, admin.AliquotAdmin)
+        filter = AliquotPatientFilter(None,
+            {'receive__patient__protocol': self.patient.protocol},
+            Aliquot, AliquotAdmin
+        )
         aliquot = filter.queryset(None, Aliquot.objects.all())[0]
         self.assertEqual(aliquot.patient.protocol, self.patient.protocol)
